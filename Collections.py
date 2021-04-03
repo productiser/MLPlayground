6b59047a375a09628f79454d39ec5b7818da4fea
Prevs <springin81@gmail.com>
2021-04-03 16:47:39 +0100

    Updated

--- Collections.py
+++ Collections.py
@@ -1,14 +1,18 @@
+from collections import defaultdict, Counter
+
 sentence = "Peter Piper picked a peck of pickled peppers\
 A peck of pickled peppers Peter Piper picked\
 If Peter Piper picked a peck of pickled peppers\
 Whereas the peck of pickled peppers Peter Piper picked"
 
-word_dict = {}
+word_dict = defaultdict(int)
 
 for word in sentence.split():
-    if(word not in word_dict):
-        word_dict[word] = 1
-    else:
-        word_dict[word]+=1
+    word_dict[word] += 1
 
 print(word_dict)
+for key, value in word_dict.items():
+    print(key, value)
+
+word_count = Counter(sentence.split())
+print(word_count)
